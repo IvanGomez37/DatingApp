e224b963529aa7339c58e55bf9f6ed3510e770c9
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2024-10-04 07:20:33 -0600

    Parcial01 - Code for first evaluation (#1)
    * Parcial01 - Initial configuration - 28/8
    
    * Parcial01 - First Entitiy 29/08
    
    * Entitis Frameworks core and Add Data Context
    
    * DB migrations
    
    * fix and first get
    
    * add users controller
    
    * add async and await
    
    * Extras Angular
    
    * New app 4/09
    
    * parcial 01 - Cors 5/09
    
    * mostrar users con @for
    
    * Parcial 01 - Certificados
    
    * Password Hash and Salt
    
    * Parcial 01 - New api/v1
    
    * Parcial01 - Account Controller and RegisterDTO
    
    * Parcial 01 - User Exists and launch.json
    
    * Parcial 01 - New Launch and Debug
    
    * Parcial 01 - Login Ready
    
    * Parcial 01 -  Add Token Service
    
    * Parcial01 - Add Token service FULL
    
    * Parcial 01 -Add authn middleware
    
    * Parcial 01 - ExtensionMethods
    
    * Parcial 01 - liitle fix
    
    * Parcial 01 - liitle fix
    
    Parcial 01 - liitle fix
    
    * Parcial 01 - Nav
    
    * Parcial 01 - NgModel
    
    * Parcial 01 - Login in Angular
    
    * Parcial 01 - Nav IF
    
    * Parcial 01 - Cert
    
    * Parcial 01 - Dropdown Menu
    
    * Parcial 01 - Persist the login
    
    * Parcial 01 - Add Home Page
    
    * Parcial 01 - New Names
    
    * Parcial 01 - Add Register Component
    
    * Parcial 01 - Parent to child organization
    
    * Parcial 01 - CHILD to PARENT communication Cancel Event
    
    * Parcial 01 - Add user registration and fix error in nav

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<UserResponse>> RegisterAsync(RegisterRequest request)
+    {   
+        if (await UserExistsAsync(request.username))
+        {
+            return BadRequest("Username already exists");
+        }
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = request.username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return new UserResponse
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserResponse>> LoginAsync(LoginRequest request)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x =>
+            x.UserName.ToLower() == request.username.ToLower());
+        
+        if (user == null)
+        {
+            return Unauthorized("Invalid username or password");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid username or password");
+            }
+        }
+
+        return new UserResponse
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+
+    }
+
+    private async Task<bool> UserExistsAsync(string username) =>
+        await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+}
