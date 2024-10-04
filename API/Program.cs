fff6af158ab4c980aec20262f2488edc9ecb8a27
Ivan Gomez <coloquila@gmail.com>
2024-10-04 07:20:33 -0600

    Parcial01 - Code for first evaluation (#1)
    * Parcial01 - Initial configuration - 28/8
    
    * Parcial01 - First Entitiy 29/08
    
    * Entitis Frameworks core and Add Data Context
    
    * DB migrations
    
    * fix and first get
    
    * add users controller
    
    * add async and await
    
    * Extras Angular
    
    * New app 4/09
    
    * parcial 01 - Cors 5/09
    
    * mostrar users con @for
    
    * Parcial 01 - Certificados
    
    * Password Hash and Salt
    
    * Parcial 01 - New api/v1
    
    * Parcial01 - Account Controller and RegisterDTO
    
    * Parcial 01 - User Exists and launch.json
    
    * Parcial 01 - New Launch and Debug
    
    * Parcial 01 - Login Ready
    
    * Parcial 01 -  Add Token Service
    
    * Parcial01 - Add Token service FULL
    
    * Parcial 01 -Add authn middleware
    
    * Parcial 01 - ExtensionMethods
    
    * Parcial 01 - liitle fix
    
    * Parcial 01 - liitle fix
    
    Parcial 01 - liitle fix
    
    * Parcial 01 - Nav
    
    * Parcial 01 - NgModel
    
    * Parcial 01 - Login in Angular
    
    * Parcial 01 - Nav IF
    
    * Parcial 01 - Cert
    
    * Parcial 01 - Dropdown Menu
    
    * Parcial 01 - Persist the login
    
    * Parcial 01 - Add Home Page
    
    * Parcial 01 - New Names
    
    * Parcial 01 - Add Register Component
    
    * Parcial 01 - Parent to child organization
    
    * Parcial 01 - CHILD to PARENT communication Cancel Event
    
    * Parcial 01 - Add user registration and fix error in nav

--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,22 @@
+using API.Extensions;
+//dotnet watch --no-hot-reload 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+//Configure the hhtp request pipeline
+app.UseCors(policy => policy
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200", 
+        "https://localhost:4200"));
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
