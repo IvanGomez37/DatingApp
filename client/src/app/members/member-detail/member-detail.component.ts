17b448515db612151c68de0ce94f4bf45f1fadc2
Ivan Gomez <ivangomez.7321@gmail.com>
2024-10-28 07:58:03 -0600

    Parcial 03 - Members Card (need fix)

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../services/members.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: any;
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+      if (!username) return;
+      this.memberService.getMember(username).subscribe({
+        next: (memberParam) => this.member= memberParam
+      });
+  }
 
 }
