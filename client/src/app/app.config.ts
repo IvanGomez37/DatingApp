45e9eea30cd9854908b1e79fb05897052de98540
Ivan Gomez <ivangomez.7321@gmail.com>
2024-11-05 07:37:09 -0600

    Parcial 03 - NgxSpinnerModule

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,22 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './interceptors/error.interceptor';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
 import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { loadingInterceptor } from './interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: "toast-bottom-right"
     }),
+    importProvidersFrom(NgxSpinnerModule),
       {
         provide: GALLERY_CONFIG,
         useValue: {
