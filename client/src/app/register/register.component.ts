e223a7e91c2be8109325cc484d6c3f99920b5d46
Ivan Gomez <ivangomez.7321@gmail.com>
2025-02-13 07:38:40 -0600

    Parcial04  - ReactiveForms

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,21 +1,44 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule, ValidatorFn, AbstractControl } from '@angular/forms';
 import { AccountService } from '../services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   private accountService = inject(AccountService)
-  //usersFromHomeComponent = input.required<any>();
   private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl("", Validators.required),
+      password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl("", [Validators.required, this.matchValues('password')])
+    })
+
+    this.registerForm.controls["password"].valueChanges.subscribe(() => {
+      this.registerForm.controls["confirmPassword"].updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
 
   register() {
     this.accountService.register(this.model).subscribe({
