1c0f1bf555036dbb75d89f0f12c7c2eaad79b972
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2025-02-27 13:02:50 -0600

    Parcial05 - Add additional filters

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -17,5 +17,7 @@ public class UserParams
         set => pageSize = (value is < 1 or > MAX_PAGE_SIZE) ? MAX_PAGE_SIZE : value;
     }
     public string? Gender { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 100;
     public string? CurrentUsername { get; set; }
 }
