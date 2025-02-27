ded0fe31abf50588cd4a194145d8830249d1cbfe
Ivan Gomez <ivangomez.7321@gmail.com>
2025-02-27 12:36:10 -0600

    Parcial05 - Add client pagination Part 1

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../models/member';
 import { of, tap } from 'rxjs';
 import { Photo } from '../models/photo';
+import { PaginatedResult } from '../models/pagination';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,23 @@ export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
   members = signal<Member[]>([]);
+  paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
 
-  getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + "users").subscribe({
-      next: members => this.members.set(members)
+  getMembers(pageNumber?: number, pageSize?: number) {
+    let params = new HttpParams();
+
+    if (pageNumber && pageSize) {
+      params = params.append("pageNumber", pageNumber);
+      params = params.append("pageSize", pageSize);
+    }
+
+    return this.http.get<Member[]>(this.baseUrl + "users", { observe: "response", params }).subscribe({
+      next: response => {
+        this.paginatedResult.set({
+          items: response.body as Member[],
+          pagination: JSON.parse(response.headers.get("Pagination")!)
+        });
+      }
     });
   }
 
