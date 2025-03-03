5dc1c8d9040dc630791fc775a6855173d937006a
Ivan Gomez <ivangomez.7321@gmail.com>
2025-03-03 07:47:19 -0600

    Parcial05 - Add sort by to the API

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -19,5 +19,6 @@ public class UserParams
     public string? Gender { get; set; }
     public int MinAge { get; set; } = 18;
     public int MaxAge { get; set; } = 100;
+    public string OrderBy { get; set; } = "lastActive";
     public string? CurrentUsername { get; set; }
 }
