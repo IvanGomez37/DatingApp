db3e4379508cebb328f900d56b07dc5e30a1aa6e
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2025-03-03 07:36:08 -0600

    Parcial05 - Clean up member service

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -24,14 +24,14 @@ export class MemberDetailComponent implements OnInit {
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
       if (!username) return;
-      this.memberService.getMember(username).subscribe({
+      /* this.memberService.getMember(username).subscribe({
         next: (member) => {
           this.member = member;
           member.photos.map((photo) => {
             this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
           });
         }
-      });
+      }); */
   }
 
 }
