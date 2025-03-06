3f91511e986b113d1997bfc9980edd3453dab629
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2025-03-06 10:11:50 -0600

    Parcial05 - Optimize action filter

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -1,12 +1,20 @@
+using System.Globalization;
 using System.Security.Claims;
 
 namespace API.Extensions;
 
 public static class ClaimsPrincipleExtensions
 {
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new ArgumentException("Cannot get the userId from token"), CultureInfo.InvariantCulture);
+
+        return userId;
+    }
     public static string GetUsername(this ClaimsPrincipal user)
     {
-        var username = user.FindFirstValue(ClaimTypes.NameIdentifier)?? throw new ArgumentNullException("No user found in token");
+        var username = user.FindFirstValue(ClaimTypes.Name)?? throw new ArgumentNullException("No user found in token");
 
         return username;
     }
