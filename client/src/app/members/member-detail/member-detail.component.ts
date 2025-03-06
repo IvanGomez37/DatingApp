6c5c2fe59010a70368a819505982922a9659871f
Ivan Gomez <ivangomez.7321@gmail.com>
2025-03-06 10:27:04 -0600

    Parcial05 - Add TimeAgo module for date formatting

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,11 +3,13 @@ import { MembersService } from '../../services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import {TabsModule} from 'ngx-bootstrap/tabs'
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TimeagoModule } from 'ngx-timeago';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule],
+  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -24,14 +26,14 @@ export class MemberDetailComponent implements OnInit {
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
       if (!username) return;
-      /* this.memberService.getMember(username).subscribe({
+      this.memberService.getMember(username).subscribe({
         next: (member) => {
           this.member = member;
           member.photos.map((photo) => {
             this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
           });
         }
-      }); */
+      });
   }
 
 }
