ebe5a16bdf15c70fe4ae242daedab3e5854b9cd6
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2025-03-25 07:38:12 -0600

    Parcial05 - Add Message Inbox 1 of 2

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,16 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../services/messages.service';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { RouterModule } from '@angular/router';
+import { Message } from '../models/message';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 export class MessagesComponent implements OnInit {
   messagesService = inject(MessagesService);
-  container = "outbox";
+  container = "unread";
   pageNumber = 1;
   pageSize = 10;
 
@@ -21,6 +26,15 @@ export class MessagesComponent implements OnInit {
   loadMessages() {
     this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container);
   }
+
+  getRoute(message: Message) {
+    // if (this.container === "outbox") return `/members/${message.recipientUsername}`;
+    // else return `/members/${message.senderUsername}`;
+  
+    return this.container === "outbox"
+      ? `/members/${message.recipientUsername}`
+      : `/members/${message.senderUsername}`;
+    }
 
   pageChanged(event: any) {
     if (this.pageNumber !== event.page) {
