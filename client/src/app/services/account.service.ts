bd7f11d3e6890d4eac36b8301fcc5f77765111bc
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2025-05-16 08:22:22 -0600

    Parcial - Add SignalR on Client

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -4,6 +4,7 @@ import { map, Observable } from 'rxjs';
 import { User } from '../models/user';
 import { environment } from '../../environments/environment';
 import { LikesService } from './likes.service';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class AccountService {
 
   private http = inject(HttpClient);
   private likesService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
   roles = computed(() => {
@@ -49,10 +51,12 @@ export class AccountService {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
     this.likesService.getLikeIds();
+    this.presenceService.createHubConnection(user);
   }
 
   logout(): void {
     localStorage.removeItem('user');
     this.currentUser.set(null);
+    this.presenceService.stopHubConnection();
   }
 }
