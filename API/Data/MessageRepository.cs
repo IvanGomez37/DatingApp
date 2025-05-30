5b4f4b53352da09a24bedb83a8531b805b8c9f16
Ivan Yahir Gomez Mancilla <ivangomez.7321@gmail.com>
2025-05-30 08:23:53 -0600

    Parcial06 - Add optimizations to messages component

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -53,16 +53,16 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
             .FirstOrDefaultAsync(g => g.Name == groupName);
     public async Task<IEnumerable<MessageResponse>> GetThreadAsync(string currentUsername, string recipientUsername)
     {
-        var messages = await context.Messages
+        var query = context.Messages
              .Where(m =>
                  (m.RecipientUsername == currentUsername && !m.RecipientDeleted && m.SenderUsername == recipientUsername) ||
                  (m.RecipientUsername == recipientUsername && !m.SenderDeleted && m.SenderUsername == currentUsername)
              )
              .OrderBy(m => m.MessageSent)
              .ProjectTo<MessageResponse>(mapper.ConfigurationProvider)
-             .ToListAsync();
+             .AsQueryable();
 
-        var unreadMessages = messages
+        var unreadMessages = query
             .Where(m => m.DateRead == null && m.RecipientUsername == currentUsername)
             .ToList();
 
@@ -71,7 +71,7 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
             unreadMessages.ForEach(m => m.DateRead = DateTime.UtcNow);
         }
 
-        return messages;
+        return await query.ProjectTo<MessageResponse>(mapper.ConfigurationProvider).ToListAsync();
     }
 
     public void Remove(Message message) => context.Messages.Remove(message);
